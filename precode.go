0d9a144c6d742cfd2b1410e3c2c391d3bf091582
Khudodatov Vitalii <hudodatov.vitaly@yandex.ru>
2024-08-12 13:43:40 +0300

    solving-1

--- precode.go
+++ precode.go
@@ -5,7 +5,10 @@ import (
 	"fmt"
 	"log"
 	"sync"
+	"time"
 )
+
+var mu sync.Mutex
 
 // Generator генерирует последовательность чисел 1,2,3 и т.д. и
 // отправляет их в канал ch. При этом после записи в канал для каждого числа
@@ -13,20 +16,43 @@ import (
 // сгенерированных чисел.
 func Generator(ctx context.Context, ch chan<- int64, fn func(int64)) {
 	// 1. Функция Generator
-	// ...
+	var cnt int64 = 1
+
+	for {
+		select {
+		case ch <- cnt:
+			mu.Lock()
+			fn(cnt)
+			cnt++
+			mu.Unlock()
+
+		case <-ctx.Done():
+			close(ch)
+			return
+		}
+	}
 }
 
 // Worker читает число из канала in и пишет его в канал out.
 func Worker(in <-chan int64, out chan<- int64) {
 	// 2. Функция Worker
-	// ...
+	for {
+		num, ok := <-in
+		if !ok {
+			close(out)
+			return
+		}
+		out <- num
+		time.Sleep(1 * time.Millisecond)
+	}
 }
 
 func main() {
 	chIn := make(chan int64)
 
 	// 3. Создание контекста
-	// ...
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
 
 	// для проверки будем считать количество и сумму отправленных чисел
 	var inputSum int64   // сумма сгенерированных чисел
@@ -55,7 +81,33 @@ func main() {
 	var wg sync.WaitGroup
 
 	// 4. Собираем числа из каналов outs
-	// ...
+	// var_1
+	/*for ind, ch := range outs {
+		wg.Add(1)
+		go func(in <-chan int64, i int64) {
+			for {
+				val, ok := <-in
+				if !ok {
+					wg.Done()
+					return
+				}
+				amounts[ind]++
+				chOut <- val
+			}
+		}(ch, int64(ind))
+	}*/
+
+	//  var_2
+	for ind, ch := range outs {
+		wg.Add(1)
+		go func(in <-chan int64, i int64) {
+			defer wg.Done()
+			for val := range in { // Цикл будет читать данные из канала  до тех пор, пока канал не закроется.
+				amounts[ind]++
+				chOut <- val
+			}
+		}(ch, int64(ind))
+	}
 
 	go func() {
 		// ждём завершения работы всех горутин для outs
@@ -68,7 +120,10 @@ func main() {
 	var sum int64   // сумма чисел результирующего канала
 
 	// 5. Читаем числа из результирующего канала
-	// ...
+	for num := range chOut {
+		count++
+		sum += num
+	}
 
 	fmt.Println("Количество чисел", inputCount, count)
 	fmt.Println("Сумма чисел", inputSum, sum)
@@ -76,7 +131,7 @@ func main() {
 
 	// проверка результатов
 	if inputSum != sum {
-		log.Fatalf("Ошибка: суммы чисел не равны: %d != %d\n", inputSum, sum)
+		log.Fatalf("Ошибка: суммы чисел не равны: %d != %d\t разница = %d\n", inputSum, sum, inputSum-sum)
 	}
 	if inputCount != count {
 		log.Fatalf("Ошибка: количество чисел не равно: %d != %d\n", inputCount, count)
